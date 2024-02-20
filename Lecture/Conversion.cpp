9091235c324f71d36eab729c6e48d63a91156bc6
ironspec07 <ironspec07@gmail.com>
2024-02-20 22:12:47 +0530

    Day 2 : Number System COnversions

---
+++ Lecture/Conversion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 0;
+    int ans = 0;
+    // Integer to Binary.
+    // while (n != 0)
+    // {
+    //     int bit = n & 1;
+    //     ans = (bit * pow(10, i)) + ans;
+
+    //     n = n >> 1;
+    //     i++;
+    // }
+    // cout << " Answer is: " << ans << endl;
+    // Binary to Integer.
+    while (n != 0)
+    {
+        int digit = n % 10;
+        if (digit == 1)
+        {
+            ans = ans + pow(2,i);
+        }
+        n = n / 10;
+        i++;
+    }
+    cout << " Answer is: " << ans << endl;
+}
