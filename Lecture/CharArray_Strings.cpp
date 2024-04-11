481a7f7f61e24d236c562d89df8c765b4b7b8351
ironspec07 <ironspec07@gmail.com>
2024-04-11 22:07:28 +0530

    day 14

--- Lecture/CharArray_Strings.cpp
+++ Lecture/CharArray_Strings.cpp
@@ -12,25 +12,25 @@ char toLowerCase(char ch)
     }
 }
 
-bool checkPalindrome(char a[], int n)
-{
-    int s = 0;
-    int e = n - 1;
+// bool checkPalindrome(char a[], int n)
+// {
+//     int s = 0;
+//     int e = n - 1;
 
-    while (s <= e)
-    {
-        if (toLowerCase(a[s]) != toLowerCase(a[e]))
-        {
-            return 0;
-        }
-        else
-        {
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
+//     while (s <= e)
+//     {
+//         if (toLowerCase(a[s]) != toLowerCase(a[e]))
+//         {
+//             return 0;
+//         }
+//         else
+//         {
+//             s++;
+//             e--;
+//         }
+//     }
+//     return 1;
+// }
 
 void reverse(char name[], int n)
 {
