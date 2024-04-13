2b5007358f89e1836da0b0cf17185311dbb5d6b5
ironspec07 <ironspec07@gmail.com>
2024-04-13 23:57:59 +0530

    Array

--- practice/ArrayRev.cpp
+++ practice/ArrayRev.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-void reverse(int arr[], int n) {
+// void reverse(int arr[], int n) {
 
-    int start = 0;
-    int end = n-1;
+//     int start = 0;
+//     int end = n-1;
 
-    while(start<=end) {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
-    }
-}
+//     while(start<=end) {
+//         swap(arr[start], arr[end]);
+//         start++;
+//         end--;
+//     }
+// }
 
-void printArray(int arr[], int n) {
+// void printArray(int arr[], int n) {
 
-    cout << " " << endl;    
-    for(int i=0; i<n; i++) {
-        cout << arr[i] << endl;
-    }
-    cout << endl;
-}
+//     cout << " " << endl;    
+//     for(int i=0; i<n; i++) {
+//         cout << arr[i] << endl;
+//     }
+//     cout << endl;
+// }
 
-int main() {
-    cout << "Enter size of array: " << endl;
-    int n;
-    cin >> n;
-    int arr[100];
+// int main() {
+//     cout << "Enter size of array: " << endl;
+//     int n;
+//     cin >> n;
+//     int arr[100];
 
-    cout << "Enter elements of array: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+//     cout << "Enter elements of array: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
 
-    reverse(arr, n);
-    printArray(arr, n);
+//     reverse(arr, n);
+//     printArray(arr, n);
 
-    return 0;
-}
+//     return 0;
+// }
